f298bd5f4e472decce99627d2e667705ec42428a
Chad Orwig <chad.orwig@slalom.com>
2019-12-09 09:06:12 -0500

    part 2

--- 2019/9/index.js
+++ 2019/9/index.js
@@ -6,3 +6,9 @@ const program = intCode(input, 1);
 for(let output of program) {
     console.log(output);
 }
+
+const part2 = intCode(input, 2);
+
+for(let output of part2) {
+    console.log(output);
+}
