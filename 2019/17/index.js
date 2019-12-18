cba778c0cf7c2013f2399f4d69443c6325f6cf59
Chad Orwig <chad.orwig@slalom.com>
2019-12-18 16:48:13 -0500

    part 2...did recursion...much better plan

--- 2019/17/index.js
+++ 2019/17/index.js
@@ -126,46 +126,94 @@ while(robot.turn) {
     robot.direction = newDirection(robot);
     const {steps, axis, offset} = countSteps(robot);
     instructions.push(Instruction.getInstruction(robot.turn, steps));
-    robot[axis] += (steps * offset);
+    robot[axis] += (steps * offset);                    
     robot.turn = determineTurn(robot);
 }
 
 console.log(instructions.map(i => i.toString()).join(',').length);
 
-function mostCommonInstructionCombo(count, instructions) {
-    const counter = new Map();
-    const countSetter = Maps.mapCoordinateSetter(counter);
-    const countGetter = Maps.mapCoordinateGetter(counter);
-    let max = 0;
-    let mostCommonCombo;
-    instructions.forEach((instruction, index, array) => {
-        if((index + count) < length) {
-            const slice = array.slice(index, index + count);
-            const currentCount = (countGetter(slice) || 0) + 1;
-            countSetter(slice, currentCount);
-            if(currentCount > max) {
-                max = currentCount;
-                mostCommonCombo = slice;
-            }
-        }
-    });
-
-    mostCommonCombo.comboCount = max;
-
-    return mostCommonCombo;
-}
-
 function comboMatch(comboA, comboB) {
-    if(combo.length === slice.length) {
-        return combo.reduce((goodSoFar, instruction, index) => goodSoFar && instruction === slice[index], true);
+    if(comboA.length === comboB.length) {
+        const match = comboA.reduce((goodSoFar, instruction, index) => goodSoFar && instruction === comboB[index], true);
+        return match;
     }
 }
 
-function inInstructionPool(pool, slice) {
-    for(let i = 0; i < pool.length; i++) {
-        const combo = pool[i];
-        if(comboMatch(combo, slice)) return true;
+function matchedInstructionReducer(instructions) {
+    return (match, combo, index, instructionSet) => {
+        if(match) return match;
+        if(comboMatch(combo, instructions.slice(0, combo.length))) {
+            const set = findInstructionSet(instructionSet, instructions.slice(combo.length));
+            if(set) set.path.push(index);
+            return set;
+        }
     }
-    return false;
 }
 
+function makeInstructionString(instructions) {
+    return instructions.map(i => i.toString()).join(',');
+}
+
+function addInstructionReducer(instructionSet) {
+    return (match, waste, index, instructions) => {
+        if(match) return match;
+        const newInstruction = instructions.slice(0, index + 1);
+        if(makeInstructionString(newInstruction).length > 20) return 'too long';
+        const success = findInstructionSet([...instructionSet, newInstruction], instructions.slice(index + 1));
+        if(success) {
+            success.path.push(instructionSet.length);
+            return success;
+        }
+    }
+}
+
+function findInstructionSet(instructionSet, instructions) {
+    if(instructions.length === 0) {
+        instructionSet.path = [];
+        return instructionSet;
+    }
+    const matchedSet = instructionSet.reduce(matchedInstructionReducer(instructions), false);
+    if(matchedSet) return matchedSet;
+
+    if(instructionSet.length < 3) {
+        const addInstructionWorks = instructions.reduce(addInstructionReducer(instructionSet), undefined);
+        if(addInstructionWorks !== 'too long') {
+            return addInstructionWorks;
+        }
+    }
+}
+
+function printPath(path) {
+    return path.map(v => {
+        switch(v) {
+            case 0: return 'A';
+            case 1: return 'B';
+            case 2: return 'C';
+        }
+    }).join(',');
+}
+
+const instructionSet = findInstructionSet([], instructions);
+instructionSet.path = instructionSet.path.reverse();
+console.log(printPath(instructionSet.path));
+instructionSet.map(makeInstructionString).forEach(s => console.log(s));
+
+
+function* inputFunction() {
+    const path = printPath(instructionSet.path);
+    const instructions = instructionSet.map(makeInstructionString).join('\n');
+    const commands = `${path}\n${instructions}\nn\n`;
+    console.log(commands);
+    for(let i = 0; i < commands.length; i++) {
+        yield commands.charCodeAt(i);
+    }
+}
+
+const part2 = intCode([2, ...input.slice(1)], inputFunction());
+
+let dust;
+for(let output of part2) {
+    dust = output;
+}
+
+console.log(dust);
