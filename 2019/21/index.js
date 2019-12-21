16685ccf07a4d6e9aa33479ce02d522e1f493bda
Chad Orwig <chad.orwig@slalom.com>
2019-12-21 13:10:37 -0500

    part 2

--- 2019/21/index.js
+++ 2019/21/index.js
@@ -1,6 +1,30 @@
 const input = require('./input');
 const intCode = require('../intCode');
 
+function* scriptInput(script) {
+    for(let i = 0; i < script.length; i++) {
+        yield script.charCodeAt(i);
+    }
+}
+
+function checkScript(script) {
+    const program = intCode(input, scriptInput(script));
+
+    const outputs = [];
+
+    for(let output of program) {
+        outputs.push(output);
+    }
+
+    if(outputs[outputs.length - 1] > 255) {
+        console.log(outputs[outputs.length - 1]);
+    } else {
+        const msg = outputs.map(code => String.fromCharCode(code))
+        .join('');
+        console.log(msg);
+    }
+}
+
 const part1Script = 
     `NOT A J
     NOT B T
@@ -11,24 +35,19 @@ const part1Script =
     WALK
     `;
 
-function* scriptInput(script) {
-    for(let i = 0; i < script.length; i++) {
-        yield script.charCodeAt(i);
-    }
-}
+const part2Script = 
+    `NOT A J
+    NOT B T
+    OR T J
+    NOT C T
+    OR T J
+    AND D J
+    NOT E T
+    NOT T T
+    OR H T
+    AND T J
+    RUN
+    `;
 
-const program = intCode(input, scriptInput(part1Script));
-
-const outputs = [];
-
-for(let output of program) {
-    outputs.push(output);
-}
-
-if(outputs[outputs.length - 1] > 255) {
-    console.log(outputs[outputs.length - 1]);
-} else {
-    const msg = outputs.map(code => String.fromCharCode(code))
-    .join('');
-    console.log(msg);
-}
+checkScript(part1Script);
+checkScript(part2Script);
