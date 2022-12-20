65c5bbb75950dae7ba56b94b2163f5ce914f6dde
Chad Orwig <chad.orwig@slalom.com>
2022-12-20 15:09:27 -0500

    test passes, main inccorect

--- 2022_rust/src/bin/20.rs
+++ 2022_rust/src/bin/20.rs
@@ -13,6 +13,11 @@ where T: PrimInt + FromStr, <T as FromStr>::Err: Debug {
     .map(Result::unwrap)
     
 }
+
+fn is_value<T: PartialEq + Copy> (v:T) -> impl Fn(&T) -> bool {
+  move |other: &T| other == &v
+}
+
 fn main () {
   let mut nums = get_input::<i64>()
     .map(|v| (v, false))
@@ -112,5 +117,93 @@ fn main () {
     .sum::<i64>();
 
   println!("{:?}", p1);
+
+
+  let order = get_input::<i128>()
+    .map(|v| v * 811589153)
+    .collect::<Vec<_>>();
+
+  let mut list = order
+    .clone()
+    .into_iter()
+    .collect::<LinkedList<_>>();
+
+  for _ in 0..10 {
+    // println!("{:?}", list);
+    mix(&mut list, &order);
+  }
+
+  let num_i = list.iter().collect::<Vec<_>>();
+  // println!("{:?}", list);
+
+  let v0_index = num_i.iter() 
+    .enumerate()
+    .find(|(_,v)| v == &&&0)
+    .unwrap().0;
+
+  let p2 = [1000 + v0_index,2000 + v0_index,3000 + v0_index].into_iter()
+    .map(|i|i % num_i.len())
+    .map(|i| num_i[i])
+    .sum::<i128>();
+
+  println!("{}", p2);
   
+}
+
+fn mix(list: &mut LinkedList<i128>, order: &Vec<i128>){
+  for v in order {
+    let i = list.iter()
+      .enumerate()
+      .find(|(_, v1)| v1 == &v)
+      .expect(format!("Couldn't find {}", v).as_str()).0;
+
+    let total_len = order.len() as i128 - 1;
+    let people_to_jump = v % total_len;
+    let mut other = list.split_off(i);
+
+    let val = other.pop_front()
+      .unwrap();
+
+    let right_len = other.len() as i128;
+    let left_len = list.len() as i128;
+
+
+    match people_to_jump {
+      x if (..left_len * - 1).contains(&x) => {
+        let right_jumps = x + left_len;
+
+        let right_i = right_len + right_jumps;
+        let mut temp = other.split_off(right_i as usize);
+        other.push_back(val);
+        other.append(&mut temp);
+      },
+      x if (left_len * - 1..0).contains(&x) => {
+        let left_i = left_len + x;
+        let mut temp = list.split_off(left_i as usize);
+        list.push_back(val);
+        list.append(&mut temp);
+      },
+      0 => {
+        list.push_back(val);
+      },
+      x if(1..right_len).contains(&x) => {
+        let mut temp = other.split_off(x as usize);
+        other.push_back(val);
+        other.append(&mut temp);
+      },
+      x if(right_len..).contains(&x) => {
+        let left_i = x - right_len;
+
+        let mut temp = list.split_off(left_i as usize);
+
+        list.push_back(val);
+        
+        list.append(&mut temp);
+
+      },
+      _ => panic!("Weird number to jump: {}", people_to_jump)
+    }
+    list.append(&mut other);
+  }
+
 }
